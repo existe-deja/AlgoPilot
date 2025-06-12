52a17045ed31b77fbe2f3a96088f760dd2cdc1ba
lacroute <mc.flurry@hotmail.fr>
2025-06-12 17:28:01 +0200

    build: update manifest.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,24 @@ export default defineNuxtConfig({
     enabled: true,
   },
 
+  app: {
+    head: {
+      title: 'Algo Pilot',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#3498db' },
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon-180.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/icon-512.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon-32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon-16.png' },
+        { rel: 'manifest', href: '/manifest.json' },
+      ],
+    },
+  },
+
   future: {
     compatibilityVersion: 4,
   },
@@ -17,19 +35,5 @@ export default defineNuxtConfig({
     config: {
       stylistic: true,
     },
-  },
-
-   app: {
-    head: {
-      title: 'Algo Pilot',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'theme-color', content: '#3498db' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
-    }
   },
 })
